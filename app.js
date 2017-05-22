c430eb2d2d9aec5f0b4e8c0d7a8b9ea1e6f5e8fc
Mitchell <mbouwman@medicapitalrent.com>
2017-05-22 17:28:05 +0200

    [FIX] Not the sidebar but in the table..

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 (function() {
   var app = angular.module('invoicesSystem', ['invoice-directives']);
 
-	  app.controller('invoicesController', ['$http', function ($http) {
+	app.controller('invoicesController', ['$http', '$scope', function ($http, $scope) {
 		var invoices = this;
 		invoices.data = [];
 		$http.get('./invoices_dummy.json').success(function (data) {
 			invoices.data = data;
 		});
-	  }]);
+		
+		
+		$scope.test = function(orderId){
+			
+			console.log(invoices.data);
+			return openDetails = true;
+		}
 
-
+		
+	}]);	
+	
+	
+	
 	
 })();
