e0ec5c89e770623e9ad969f5203fab12814e6607
Mitchell <mbouwman@medicapitalrent.com>
2017-05-26 15:54:44 +0200

    some fixes

--- app.js
+++ app.js
@@ -4,21 +4,17 @@
 	app.controller('invoicesController', ['$http', '$scope', '$compile', function ($http, $scope, $compile) {
 		var invoices = this;
 		invoices.details = [];
-		var invoice = [];
-		var invoicesData = null;
-		
+		invoices.invoice = [];	
 		
 		invoices.details = [{"invoiceId": 1,"name": "AbbVie France","invoiceDate": "1288323623006"},
 							{"invoiceId": 2,"name": "Ablynx NV","invoiceDate": "1288323623006"},
 							{"invoiceId": 3,"name": "Astellas Pharma Europe BV","invoiceDate": "1288323623006"}
 						   ];	
-		invoice		 	 = [{"invoiceId": 1,"name": "AbbVie France","invoiceDate": "1288323623006","invoices": [{"product": "Computer screen 27inch","price": "279.95","date": "1288323623006"},{"product": "Heart Sensor","price": "1199.95","date": "1288323623003"}]},{"invoiceId": 2,"name": "Ablynx NV","invoiceDate": "1288323623006","invoices": [{"product": "10 pack needles","price": "23.99","date": "1288323623003"},{"product": "2x 13mm fans","price": "26.98","date": "1288323623003"}]},{"invoiceId": 3,"name": "Astellas Pharma Europe BV","invoiceDate": "1288323623006","invoices": [{"product": "10 pack needles","price": "23.99","date": "1288323623003"},{"product": "10 pack needles","price": "23.99","date": "1288323623003"},{"product": "10 pack needles","price": "23.99","date": "1288323623003"},{"product": "2x 13mm fans","price": "26.98","date": "1288323623003"}]}]
-		
-		console.log(invoice);
+		invoices.invoice	 = [{"invoiceId": 1,"name": "AbbVie France","invoiceDate": "1288323623006","invoices": [{"product": "Computer screen 27inch","price": "279.95","date": "1288323623006"},{"product": "Heart Sensor","price": "1199.95","date": "1288323623003"}]},{"invoiceId": 2,"name": "Ablynx NV","invoiceDate": "1288323623006","invoices": [{"product": "10 pack needles","price": "23.99","date": "1288323623003"},{"product": "2x 13mm fans","price": "26.98","date": "1288323623003"}]},{"invoiceId": 3,"name": "Astellas Pharma Europe BV","invoiceDate": "1288323623006","invoices": [{"product": "10 pack needles","price": "23.99","date": "1288323623003"},{"product": "10 pack needles","price": "23.99","date": "1288323623003"},{"product": "10 pack needles","price": "23.99","date": "1288323623003"},{"product": "2x 13mm fans","price": "26.98","date": "1288323623003"}]}]
 		
 		
 		/* Click button "View invoices" */
-		$scope.getData = function(order, $index){	
+		$scope.getData = function(order, $index){
 			var colSpan = $('table.invoices > thead > tr > th').length;
 			
 			/* Nice toggling between customers invoices */
@@ -30,7 +26,6 @@
 				$("table.invoices #invoicesTbody").find('#orderwrap').remove();
 			}
 			/* // Nice toggling between customers invoices */
-			
 			
 				var tbody = document.getElementById('invoicesTbody');	// Get tbody.
 				
@@ -44,31 +39,13 @@
 				cell.colSpan = colSpan;
 
 				/* Parse the variable to the invoice-rows element */
-				$scope.invoiceDetails = invoice[$index];
+				$scope.invoiceDetails = invoices.invoice[$index];
 
 				/* Append the directive into the cell and compile it */
 				$(cell).append($compile('<invoice-rows details="invoiceDetails"></invoice-rows>')($scope));
 			
 			
 		} /* // getData function */
-		
-		
-		/*
-		
-		$scope.newInvoice = function(orderId){		
-			// Make the form
-			var form = "<form><div class=\"form-group\"><label for=\"description\" class=\"col-sm-12 control-label text-left\">Description:</label><div class=\"col-sm-12\"><input type=\"text\" class=\"form-control\" id=\"description\" value=\"\"></div></div><br class=\"clearfix\" clear=\"all\" /><br class=\"clearfix\" clear=\"all\" /><div class=\"form-group\"><label for=\"quantity\" class=\"col-sm-12 control-label\">Quantity</label><div class=\"col-sm-12\"><input class=\"form-control\" id=\"quantity\" value=\"\"></div></div><br class=\"clearfix\" clear=\"all\" /><br class=\"clearfix\" clear=\"all\" /><div class=\"form-group\"><label for=\"amount\" class=\"col-sm-12 control-label\">Amount</label><div class=\"col-sm-12\"><input class=\"form-control\" id=\"amount\" value=\"\"></div></div><br class=\"clearfix\" clear=\"all\" /><br class=\"clearfix\" clear=\"all\" /><div class=\"form-group\"><div class=\"col-sm-12 text-right\"><button type=\"submit\" class=\"btn btn-success\">Add transaction</button></div></div></form>";
-			
-			// Empty the div.
-			$("#addInvoice .modal-body .appendHere").empty();
-			// Append it to the modal.
-			$("#addInvoice .modal-body .appendHere").append(form);
-			
-			// Last but not least, open the modal.
-			$("#addInvoice").modal();
-		} 
-		*/
-		//#addInvoice .modal-body .appendHere
 		
 		
 	}]); // invoicesController	
--- static/js/directives/invoiceData.js
+++ static/js/directives/invoiceData.js
@@ -1,18 +1,21 @@
 (function(){
-    var app = angular.module('invoice-directives', []);		
+    var app = angular.module('invoice-directives', ['ngInputDate']);		
 	
 	app.directive('invoiceRows', function (){
 		function controller($scope, $compile){
-			console.log($scope.details);			
+			$scope.dateBirth = new Date();			
 			
 			/* Edit product button */
 			$scope.editProduct = function($index){
 				$scope.productDetails = {};
 				
-
 				/* Create new one */
 				if(!$index && $index != 0){
 					$("#invoicemodal .modal-body").empty();
+					
+					// ProductID to "new" so a "save" is a new entry.
+					$scope.productDetails['productId'] = 'new';
+					$scope.productDetails['invoiceId'] = $scope.details['invoiceId'];
 					
 					// Append compiled data to InvoiceModal.
 					$("#invoicemodal .modal-body").append($compile('<edit-product data="productDetails"></edit-product>')($scope));
@@ -38,7 +41,11 @@
 				}
 			
 			}/* // editProduct */
-			
+			$scope.deleteProduct = function($index){
+				
+				// Delete from JSON array.
+				$scope.details.invoices.splice($index, 1);
+			}
 		}
 		return {
 			restrict: 'E',
@@ -54,16 +61,27 @@
 	app.directive('editProduct', function (){
 		function controller($scope, $compile){
 			
-			$scope.submitProductForm = function($index){
+			console.log($scope);	
+			
+			
+			$scope.submitProductForm = function($scope, invoices){
 				var productForm = $("#invoicemodal .modal-body form");
 				
-				productForm.submit(function(e){
+				productForm.submit(function(e, $scope){
 					e.preventDefault();
-					
-					var formdata = productForm.serialize();
+					var formdata =  $.deparam(productForm.serialize());
 					console.log(formdata);
 					
+					
+					if(formdata.productId == "new"){
+						//alert("Save new product");
+					} else {						
+						//alert("Save existing product");
+					}
+					
 				});
+				
+				
 				$(productForm).submit();				
 			}			
 		}
@@ -82,4 +100,3 @@
 	
 	
 })();
-
