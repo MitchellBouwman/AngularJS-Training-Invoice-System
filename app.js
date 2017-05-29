c91ff9a5a9b76e002d3ca2e0af65df421c27a892
Mitchell <mbouwman@medicapitalrent.com>
2017-05-29 13:00:12 +0200

    [FIX] Editing and making a new product.

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 (function() {
-  var app = angular.module('invoicesSystem', ['invoice-directives']);
+  var app = angular.module('invoicesSystem', []);
 
-	app.controller('invoicesController', ['$http', '$scope', '$compile', function ($http, $scope, $compile) {
-		var invoices = this;
-		invoices.details = [];
-		invoices.invoice = [];	
-		
+	
+
+	
+	app.controller('invoicesController', ['$http', '$scope', '$rootScope', '$compile', function ($http, $scope, $rootScope, $compile) {
+		var invoices = this;		
 		invoices.details = [{"invoiceId": 1,"name": "AbbVie France","invoiceDate": "1288323623006"},
 							{"invoiceId": 2,"name": "Ablynx NV","invoiceDate": "1288323623006"},
 							{"invoiceId": 3,"name": "Astellas Pharma Europe BV","invoiceDate": "1288323623006"}
 						   ];	
 		invoices.invoice = [{"invoiceId": 1,"name": "AbbVie France","invoiceDate": "1288323623006","products": [{"product": "Computer screen 27inch","price": "279.95","date": "1288323623006"},{"product": "Heart Sensor","price": "1199.95","date": "1288323623003"}]},{"invoiceId": 2,"name": "Ablynx NV","invoiceDate": "1288323623006","products": [{"product": "10 pack needles","price": "23.99","date": "1288323623003"},{"product": "2x 13mm fans","price": "26.98","date": "1288323623003"}]},{"invoiceId": 3,"name": "Astellas Pharma Europe BV","invoiceDate": "1288323623006","products": [{"product": "10 pack needles","price": "23.99","date": "1288323623003"},{"product": "10 pack needles","price": "23.99","date": "1288323623003"},{"product": "10 pack needles","price": "23.99","date": "1288323623003"},{"product": "2x 13mm fans","price": "26.98","date": "1288323623003"}]}];
-		
-		$scope.invoices = invoices.invoice;
+		$rootScope.invoices = invoices.invoice;
+	
 		
 		/* Click button "View invoices" */
 		$scope.getData = function(order, $index){
@@ -48,9 +48,111 @@
 			
 			
 		} /* // getData function */
-		
-
-		
-		
 	}]); // invoicesController	
+	
+	
+	
+	
+	app.directive('invoiceRows', function (){
+		function controller($scope, $compile){
+			$scope.dateBirth = new Date();			
+			
+			/* Edit product button */
+			$scope.editProduct = function($index){
+				$scope.productDetails = {};
+				
+				/* Create new one */
+				if(!$index && $index != 0){
+					$("#invoicemodal .modal-body").empty();
+					
+					// ProductID to "new" so a "save" is a new entry.
+					$scope.productDetails['productId'] = 'new';
+					$scope.productDetails['invoiceId'] = $scope.details['invoiceId'];
+					
+					// Append compiled data to InvoiceModal.
+					$("#invoicemodal .modal-body").append($compile('<edit-product data="productDetails"></edit-product>')($scope));
+					
+					// Show modal.
+					$("#invoicemodal").modal();
+				} else {
+				/* Edit excisting one */
+					// Empty modal body first.
+					$("#invoicemodal .modal-body").empty();
+					
+					// Make product object.
+					$scope.productDetails = $scope.details.products[$index];
+					// Add productId & invoiceId so it knows where to save.
+					$scope.productDetails['productId'] = $index;
+					$scope.productDetails['invoiceId'] = $scope.details['invoiceId'];
+					
+					// Append compiled data to InvoiceModal.
+					$("#invoicemodal .modal-body").append($compile('<edit-product data="productDetails"></edit-product>')($scope));
+
+					// Show modal.
+					$("#invoicemodal").modal();
+				}
+			
+			}/* // editProduct */
+			
+			// Delete row/product from invoice.
+			$scope.deleteProduct = function($index){
+				// Delete from JSON array.
+				$scope.details.products.splice($index, 1);
+			}
+		}
+		return {
+			restrict: 'E',
+			scope: {
+				details: '='
+			},
+			templateUrl: 'invoiceRows.html',
+			controller: controller
+		}
+	});
+	
+	app.directive('editProduct', ['$http', '$rootScope', '$compile', function ($http, $rootScope, $compile) {
+		function controller($scope, $compile){
+			
+			$scope.submitProductForm = function($scope){
+				var productForm = $("#invoicemodal .modal-body form");			// Get the form
+				var invoicesJSON = $rootScope.invoices;			// Get the product JSON
+
+				productForm.submit(function(e, $scope){
+					e.preventDefault();											// Prevent what you normally do with a form (post & refresh page).
+					var formdata =  $.deparam(productForm.serialize());			// Get all data from the submitted form.
+					var invoiceNo = formdata.invoiceId - 1;
+					var productId = formdata.productId;
+					
+					if(formdata.productId == "new"){						
+						invoicesJSON[invoiceNo].products.push({
+							"date": formdata.date, 
+							"product": formdata.product,
+							"price": formdata.price
+						});
+						
+						$("#invoicemodal").modal('hide');
+						
+					} else {						
+						console.log("Saving existing product");
+						$("#invoicemodal").modal('hide');
+					}
+					
+				});
+				
+				$rootScope.invoices = invoicesJSON; // Update the invoices rootscope.
+				$(productForm).submit();	 		// Submit the form.. run above function - "productForm.submit".	
+			}			
+		}
+		
+		return {
+			restrict: 'E',
+			scope: {
+				data: '='
+			},
+			templateUrl: 'editProduct.html',
+			controller: controller
+		}
+	}]);
+	
+	
 })();
